79ef2b40b5d0a8c729061bf65ea74bb23c6149c3
POP303U <nicoanton.gering@gmail.com>
2023-07-09 20:13:01 +0200

    [1.2] Automatic Steam path scanner

--- extractor.py
+++ extractor.py
@@ -1,5 +1,39 @@
 import os
 import shutil
+import platform
+import string
+
+def get_steam_installation_directory():
+    system = platform.system()
+    
+    if system == "Windows":
+        # Check if the Steam installation directory exists in the default location
+        default_directory = os.path.expanduser("~\\Program Files (x86)\\Steam")
+        if os.path.exists(default_directory):
+            return default_directory
+    
+        # Search for the Steam installation directory on all available drives
+        drives = ["{}:".format(d) for d in string.ascii_uppercase if os.path.exists("{}:\\".format(d))]
+        for drive in drives:
+            possible_directory = os.path.join(drive, "Program Files (x86)\\Steam")
+            if os.path.exists(possible_directory):
+                return possible_directory
+
+    elif system == "Darwin":
+        steam_directory = os.path.expanduser("~/Library/Application Support/Steam")
+        if os.path.exists(steam_directory):
+            return steam_directory
+        else:
+            return None
+    
+    elif system == "Linux":
+        steam_directory = os.path.expanduser("~/.steam/steam")
+        if os.path.exists(steam_directory):
+            return steam_directory
+        else:
+            return None
+    
+    return None
 
 def extract_images(song_directory, output_directory, scrape_type):
     print("Scraping...")
@@ -25,7 +59,7 @@ def extract_images(song_directory, output_directory, scrape_type):
 def take_input():
     print("\x1B[2J\x1B[1;1H");
     print("#-----------------------------------------------------------#");
-    print("|               quaver-scraper [version 1.11]               |");
+    print("|               quaver-scraper [version 1.2]                |");
     print("#-----------------------------------------------------------#");
     scrape_type = input("What type of files do you want to scrape: (.png, .mp3, .qua): ");
     while True:
@@ -34,10 +68,9 @@ def take_input():
         else:
             print("Invalid file type, please choose the valid file types listed above.");
     output_dir = input("Enter output directory, example: (C:\Extractor): ");
-    input_dir = input("Enter input directory, leave blank if Quaver is installed through Steam: ");
-    if len(input_dir) == 0:
-        input_dir = "C:\Program Files (x86)\Steam\steamapps\common\Quaver\Songs"
-    extract_images(input_dir, output_dir, scrape_type);
+
+    steam_directory = get_steam_installation_directory() + "\steamapps\common\Quaver\Songs"
+    extract_images(steam_directory, output_dir, scrape_type);
     if input("Exit? (y/n): ") != "y":
         take_input();
 
