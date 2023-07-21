2fc75ef6e2a3442e64de51a26d4804606effd7dc
POP303U <nicoanton.gering@gmail.com>
2023-07-21 22:16:21 +0200

    [1.3] Kotlin improvements

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -6,19 +6,17 @@ val steamInstallationDirectory: String?
         System.getProperty("os.name").contains("win", ignoreCase = true) -> {
             val defaultDirectory = Paths.get(System.getenv("ProgramFiles(x86)"), "Steam").toString()
             if (Files.exists(Paths.get(defaultDirectory))) defaultDirectory
-            else {
-                val driveLetters = ('A'..'Z').map { "$it:" }.toTypedArray()
-                driveLetters.map {
-                    Paths.get(it, "Program Files (x86)", "Steam").toString()
-                }.firstOrNull { Files.exists(Paths.get(it)) }
+            else ('A'..'Z').firstNotNullOfOrNull {
+                val path = Paths.get("$it:", "Program Files (x86)", "Steam")
+                if (Files.exists(path)) path.toString() else null
             }
         }
         System.getProperty("os.name").contains("mac", ignoreCase = true) -> Paths.get(System.getProperty("user.home"), "Library", "Application Support", "Steam").toString()
         else -> Paths.get(System.getProperty("user.home"), ".steam", "steam").toString()
     }
 
-fun sanitizeFilename(filename: String?): String = filename!!.replace("[<>:\"/\\\\|?*]".toRegex(), "")
-fun getQuaTitle(quaFilePath: String): String = File(quaFilePath).useLines { it.drop(6).firstOrNull()?.substring(7)?.trim() ?: "" }
+fun String.sanitizeFilename() = replace(Regex("[<>:\"/\\\\|?*]"), "")
+fun getQuaTitle(quaFilePath: String) = File(quaFilePath).useLines { it.drop(6).firstOrNull()?.substring(7)?.trim() ?: "" }
 
 fun main() {
     print("\u001b[H\u001b[2J")
@@ -27,9 +25,9 @@ fun main() {
     println("#-----------------------------------------------------------#")
     print("What type of files do you want to scrape: (Images, Music): ")
 
-    val allowedExtension = when (readlnOrNull()?.trim()) {
-        "Images" -> "\\.(jpg|png|jpeg)$"
-        "Music" -> "\\.(mp3)$"
+    val allowedExtension = when (readlnOrNull()?.trim()?.lowercase()) {
+        "images" -> "\\.(jpg|png|jpeg)$"
+        "music" -> "\\.(mp3)$"
         else -> {
             println("Invalid file type, please choose the valid file types listed above.")
             return
@@ -37,7 +35,7 @@ fun main() {
     }
 
     print("Enter output directory, example: (C:Extractor): ")
-    val destinationDirectory = BufferedReader(FileReader(FileDescriptor.`in`)).readLine().trim { it <= ' ' }
+    val destinationDirectory = readlnOrNull()?.trim { it <= ' ' } ?: "C:Extractor"
     val rootDirectory = steamInstallationDirectory?.let { Paths.get(it, "steamapps", "common", "Quaver", "Songs").toString() }
     var quaTitleName = ""
 
@@ -48,8 +46,8 @@ fun main() {
             val fileName = path.fileName.toString()
             val extension = fileName.substring(fileName.lastIndexOf(".")).lowercase()
             when {
-                extension == ".qua" -> quaTitleName = sanitizeFilename(getQuaTitle(path.toString()))
-                extension.matches(allowedExtension.toRegex()) -> {
+                extension == ".qua" -> quaTitleName = getQuaTitle(path.toString()).sanitizeFilename()
+                extension.matches(Regex(allowedExtension)) -> {
                     val destinationPath = Paths.get(destinationDirectory, quaTitleName + extension)
                     Files.copy(path, destinationPath, StandardCopyOption.REPLACE_EXISTING)
                 }
